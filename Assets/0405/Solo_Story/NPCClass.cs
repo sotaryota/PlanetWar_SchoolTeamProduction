9176eecd8deabca713a0407ede3869af84baab3c
sotaryota <21ci0422@jec.ac.jp>
2022-11-28 03:00:44 +0900

    変更途中、難しい

--- Assets/0405/Solo_Story/NPCClass.cs
+++ Assets/0405/Solo_Story/NPCClass.cs
@@ -5,21 +5,36 @@ public class NPCClass : MonoBehaviour
     //エディター用にpublicにしています
     //読み取り書き換えは基本getsetを使え
     [Header("NPCプロパティ")]
-    public int      id;           //ID
-    public bool     talkFlag;     //話しかけられるフラグ
-    public string   name;         //名前
-    public string[] talk;         //会話内容
+    public int       id;           //ID
+    public bool      talkFlag;     //話しかけられるフラグ
+    public string    name;         //名前
 
-    public enum TalkProperty
+#pragma warning disable CS8632 // '#nullable' 注釈コンテキスト内のコードでのみ、Null 許容参照型の注釈を使用する必要があります。
+    public string?[] normalTalk;   //通常会話内容
+    public string?[] battlelTalk;  //戦闘会話内容
+    public string?[] friendTalk;   //友好会話内容
+#pragma warning restore CS8632 // '#nullable' 注釈コンテキスト内のコードでのみ、Null 許容参照型の注釈を使用する必要があります。
+
+    public enum NPCState
     { 
-        Normal, //通常会話
-        Select, //セレクト状態
-        Battle, //バトル状態
-        Friend, //友好状態
-        End,    //会話終了
+        Normal,   //通常会話
+        Select,   //セレクト状態
+        Battle,   //バトル状態
+        Friend,   //友好状態
+        End,      //会話終了
 
-        ENUMEND //enumの最終値
+        NonEvent, //イベントなし
+
+        ENUMEND   //enumの最終値
     };
+    [Header("NPCの状態")]
+    public NPCState firstState;
+    public NPCState nowState;
+
+    public NPCState GetState()
+    {
+        return nowState;
+    }
 
     //--------------------------------------
     //ゲッター
@@ -38,7 +53,7 @@ public class NPCClass : MonoBehaviour
     }
     public string[] GetTalk()
     {
-        return this.talk;
+        return this.normalTalk;
     }
 
     public void SetTalkFlag(bool flag)
--- Assets/0422/Editor/NPCClass_Editor.cs
+++ Assets/0422/Editor/NPCClass_Editor.cs
@@ -9,14 +9,151 @@ public class NPCClass_Editor : Editor
 {
     public override void OnInspectorGUI()
     {
-        serializedObject.Update();//最新状態に上書き
+        //最新状態に上書き
+        serializedObject.Update();
         NPCClass npc = target as NPCClass;
+        using (new EditorGUI.DisabledGroupScope(true))
+        {
+            EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour((MonoBehaviour)target), typeof(MonoScript), false);
+        }
+        //NPCの名前入力
+        serializedObject.FindProperty(nameof(NPCClass.name)).stringValue = EditorGUILayout.TextField("Enemy name", serializedObject.FindProperty(nameof(NPCClass.name)).stringValue);
+        //ID
+        serializedObject.FindProperty(nameof(NPCClass.id)).intValue = EditorGUILayout.IntField("ID", serializedObject.FindProperty(nameof(NPCClass.id)).intValue);
 
-        //ヘルプボックスとスクリプトアドレス
-        EditorGUILayout.HelpBox("　スクリプトを開く → 「enum EnemyName」に名前を追加 → 情報を入力", MessageType.Info);
-        DrawDefaultInspector();
-        EditorGUILayout.Space();
+        //NPCの初期状態と現在の状態表示
+        npc.firstState = (NPCClass.NPCState)EditorGUILayout.EnumPopup("初期の状態", npc.firstState);
+        EditorGUILayout.BeginHorizontal();
 
-        for(int i = 0; i  < npc.)
+        EditorGUILayout.LabelField("現在の状態");
+        EditorGUILayout.LabelField(((NPCClass.NPCState)npc.nowState).ToString(), EditorStyles.label);
+
+        EditorGUILayout.EndHorizontal();
+        //通常時の会話　一番最初に話しかけたときの内容
+        //-----------------------------------------------------------------------------------------------
+        EditorGUILayout.LabelField("通常状態の会話");
+        EditorGUILayout.BeginHorizontal();
+        //配列の長さ変更
+        if (npc.normalTalk != null)
+        {
+            if (GUILayout.Button("行を追加", GUILayout.Width(100), GUILayout.Height(20)))
+            {
+                Array.Resize(ref npc.normalTalk, npc.normalTalk.Length + 1);
+            }
+
+            if (GUILayout.Button("末尾の行を削除", GUILayout.Width(100), GUILayout.Height(20)))
+            {
+                if (npc.normalTalk.Length > 1)
+                {
+                    Array.Resize(ref npc.normalTalk, npc.normalTalk.Length - 1);
+                }
+                else
+                {
+                    npc.normalTalk = null;
+                }
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("このイベントは存在しません", GUILayout.Width(200), GUILayout.Height(20)))
+            {
+                npc.normalTalk = new string[1];
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+        if (npc.normalTalk != null)
+        {
+            for (int i = 0; i < npc.normalTalk.Length; i++)
+            {
+                npc.normalTalk[i] = EditorGUILayout.TextField((i + 1).ToString() + "行目", npc.normalTalk[i]);
+            }
+
+        }
+        //-----------------------------------------------------------------------------------------------
+        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
+        //戦闘態勢時の会話
+        //-----------------------------------------------------------------------------------------------
+        EditorGUILayout.LabelField("戦闘態勢の時の会話");
+        EditorGUILayout.BeginHorizontal();
+        //配列の長さ変更
+        if (npc.battlelTalk != null)
+        {
+            if (GUILayout.Button("行を追加", GUILayout.Width(100), GUILayout.Height(20)))
+            {
+                Array.Resize(ref npc.battlelTalk, npc.battlelTalk.Length + 1);
+            }
+
+            if (GUILayout.Button("末尾の行を削除", GUILayout.Width(100), GUILayout.Height(20)))
+            {
+                if (npc.battlelTalk.Length > 1)
+                {
+                    Array.Resize(ref npc.battlelTalk, npc.battlelTalk.Length - 1);
+                }
+                else
+                {
+                    npc.battlelTalk = null;
+                }
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("このイベントは存在しません", GUILayout.Width(200), GUILayout.Height(20)))
+            {
+                npc.battlelTalk = new string[1];
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+        if (npc.battlelTalk != null)
+        {
+            for (int i = 0; i < npc.battlelTalk.Length; i++)
+            {
+                npc.battlelTalk[i] = EditorGUILayout.TextField((i + 1).ToString() + "行目", npc.battlelTalk[i]);
+            }
+        }
+        //-----------------------------------------------------------------------------------------------
+        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
+        //友好状態の時の会話
+        //-----------------------------------------------------------------------------------------------
+        EditorGUILayout.LabelField("友好状態の時の会話");
+        EditorGUILayout.BeginHorizontal();
+        //配列の長さ変更
+        if (npc.friendTalk != null)
+        {
+            if (GUILayout.Button("行を追加", GUILayout.Width(100), GUILayout.Height(20)))
+            {
+                Array.Resize(ref npc.friendTalk, npc.friendTalk.Length + 1);
+            }
+
+            if (GUILayout.Button("末尾の行を削除", GUILayout.Width(100), GUILayout.Height(20)))
+            {
+                if (npc.friendTalk.Length > 1)
+                {
+                    Array.Resize(ref npc.friendTalk, npc.friendTalk.Length - 1);
+                }
+                else
+                {
+                    npc.friendTalk = null;
+                }
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("このイベントは存在しません", GUILayout.Width(200), GUILayout.Height(20)))
+            {
+                npc.friendTalk = new string[1];
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+        if (npc.friendTalk != null)
+        {
+            for (int i = 0; i < npc.friendTalk.Length; i++)
+            {
+                npc.friendTalk[i] = EditorGUILayout.TextField((i + 1).ToString() + "行目", npc.friendTalk[i]);
+            }
+        }
+        //-----------------------------------------------------------------------------------------------
+
+        //現在の情報を保存
+        serializedObject.ApplyModifiedProperties();
     }
 }
