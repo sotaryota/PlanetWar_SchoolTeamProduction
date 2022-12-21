6131ba30076888ed14ad7b9c5a5af81224a4045b
sotaryota <21ci0422@jec.ac.jp>
2022-12-21 16:19:55 +0900

    足音を鳴らす処理　それに伴うレイヤーの変更

--- Assets/0422/Script/Player/Solo/Scripts/PlayerGroundCheck.cs
+++ Assets/0422/Script/Player/Solo/Scripts/PlayerGroundCheck.cs
@@ -5,24 +5,47 @@ using UnityEngine;
 public class PlayerGroundCheck : MonoBehaviour
 {
     [SerializeField] PlayerJump jump;
+    [SerializeField] PlayerMove_Solo move;
     [SerializeField] PlayerAnimManeger playerAnimation;
     public bool isGround;  //接地判定
+    [Header("レイヤー")]
+    [SerializeField] LayerMask iron;
+    [SerializeField] LayerMask sand;
+    public AudioClip ironMoveSE;
+    public AudioClip sandMoveSE;
+    public bool ironGround = false;
+    public bool sandGround = false;
 
     //地面との接触判定
     private void OnTriggerEnter(Collider other)
     {
+        print("Enter");
         if (other.gameObject.tag == "Ground")
         {
             jump.jumping.y = 0;
         }
     }
+
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Ground")
         {
+            print("stay");
+            print("obj : " + other.gameObject.name);
+            print("layer : " + other.gameObject.layer);
             isGround = true;
             //ジャンプモーション
             playerAnimation.PlayAnimSetJump(false);
+            switch (other.gameObject.layer)
+            {
+                case 6:
+                    move.moveSE = ironMoveSE;
+                    break;
+                case 3:
+                    move.moveSE = sandMoveSE;
+                    break;
+            }
         }
     }
     private void OnTriggerExit(Collider other)
